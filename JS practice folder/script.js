b4362a460fe7ecc19e2741d3dea1bf272785bb3a
Chhetri Rocks <chhetridon123@gmail.com>
2021-04-10 19:06:24 +0545

    starting another challange

--- JS practice folder/script.js
+++ JS practice folder/script.js
@@ -199,3 +199,91 @@
 // ***********************************************//
 
 // Banklist website
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnsOpenModal.forEach((btn) => {
+  btn.addEventListener('click', openModal);
+});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML =
+//   'We use cookie for improved functionality and analytics.<button class="btn btn--close--cookie">Got it</button>';
+
+// header.append(message);
+
+// document
+//   .querySelector('.btn--close--cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
+
+// smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset,
+  // );
+  // console.log(window.pageYOffset);
+  // console.log(
+  //   s1coords.left + window.pageYOffset,
+  //   s1coords.top + window.pageYOffset,
+  // );
+  // old way
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // mordern weekday
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+const random = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const randomcolor = () => {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+};
+
+console.log(randomcolor());
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomcolor();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomcolor();
+});
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomcolor();
+});
